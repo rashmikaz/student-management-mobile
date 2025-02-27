21e20409d527be595ac53f0206aefb09d2271a56
rashmikaz <phrashmika@gmail.com>
2025-02-28 04:07:56 +0530

    login

---
+++ app/index.tsx
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+import { Image,View, Text, TextInput, Button, ImageBackground, StyleSheet } from 'react-native';
+import {useRouter} from "expo-router";
+
+export default function LoginScreen() {
+
+    const router = useRouter();
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    function handleLogin(){
+        if(username === 'User' && password === 'user'){
+            router.replace('/dashboard');
+        }
+    }
+    return (
+
+        <View style={styles.container}>
+
+            <Image
+                source={require('../assets/login.jpg')} // Replace with your image path
+                style={styles.logo}
+            />
+
+            <Text style={styles.mainText}>Student Management System);</Text>
+            <Text style={styles.loginText}>Login</Text>
+            <TextInput style={styles.textFields} placeholder='Username' onChangeText={setUsername}/>
+            <TextInput style={styles.textFields} placeholder='Password' secureTextEntry onChangeText={setPassword}/>
+            <Button  onPress={handleLogin} title='Login' color="#06B6D4"/>
+            <Text style={styles.signupText}>new on our service?</Text>
+            <Text style={styles.signup}>create an account</Text>
+
+        </View>
+
+    );
+}
+
+const styles = StyleSheet.create({
+    container :{
+      flex:2,
+      justifyContent: "center",
+        padding: 20
+    },
+
+    loginText: {
+        fontSize: 24,
+        fontWeight: "semibold",
+        marginBottom: 20
+    },
+    textFields :{
+      borderWidth: 1,
+      padding: 10,
+      marginBottom : 10
+    },
+    logo: {
+        width: 350, // Image Width
+        height: 350, // Image Height
+        marginBottom: 20, // Space between image and text
+        borderRadius: 75, // Circular Image
+    },
+    mainText: {
+        fontSize: 36,
+        fontWeight: "bold",
+        position:"absolute",
+        bottom:520
+    },
+    signupText: {
+        fontSize:15,
+        fontWeight: "ultralight",
+    },
+    signup: {
+        fontSize:15,
+        fontWeight:"ultralight",
+        color:"#06B6D4",
+        position:"relative",
+        bottom:16,
+        left:140
+    },
+
+
+});
